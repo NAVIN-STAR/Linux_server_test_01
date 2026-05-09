a73e355d95734f64f539081fc4fbfe66941ac3c7
NAVIN-STAR <navinacharya2000@gmail.com>
2026-05-09 15:43:32 +0530

    add .env

--- app/main.py
+++ app/main.py
@@ -4,13 +4,20 @@ from .database import Base, engine
 from . import models
 from .routes.coffee_routes import router as coffee_router
 from .routes.auth_routes import router as auth_router
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
 
 Base.metadata.create_all(bind=engine)
-
+APP_NAME = os.getenv("APP_NAME")
 app=FastAPI(root_path="/test01")
 app.include_router(coffee_router)
 app.include_router(auth_router)
-
+@app.get("/")
+async def root():
+    return {
+        "app_name": APP_NAME
+    }
 
 
